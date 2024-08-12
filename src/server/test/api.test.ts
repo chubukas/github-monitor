80849ca4dc7101e68e46efa619766171e02a73e7
chubukas <chubukass@gmail.com>
2024-08-12 16:24:46 +0100

    added readme documentation

--- src/server/test/api.test.ts
+++ src/server/test/api.test.ts
@@ -7,6 +7,7 @@ describe("GitHub Service", () => {
     try {
       await sequelize.authenticate(); // Check if the connection is successful
       await sequelize.sync({ force: true });
+      console.log(`Database connection established and synced`);
     } catch (error) {
       console.error("Unable to connect to the database:", error);
     }
